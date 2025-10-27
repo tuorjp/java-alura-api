34faf2aa2921160acfc0a805e38ff3abc755fefc
João Pedro <jplmarotinho22@gmail.com>
2025-10-27 09:26:38 -0300

    Query da requisição dinâmica

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,14 +5,21 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o nome do filme: ");
+        var busca = scan.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=91835625";
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest
                 .newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=91835625"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse<String> response = client
